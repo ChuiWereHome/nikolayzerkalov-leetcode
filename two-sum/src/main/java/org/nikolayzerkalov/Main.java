33f18acef882af6023b837a10f42d0b789963f57
zerkalov <n.zerkalov@cft.ru>
2024-08-30 15:26:30 +0700

    update the access modifier for method

--- two-sum/src/main/java/org/nikolayzerkalov/Main.java
+++ two-sum/src/main/java/org/nikolayzerkalov/Main.java
@@ -19,7 +19,7 @@ public class Main {
   }
 
 
-  public static int[] twoSum(int[] nums, int target) {
+  private static int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
 
     for (int i = 0; i < nums.length; i++) {
